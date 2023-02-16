91f4339a8c1b6f5643096c0d2759026e71d03ec3
LarryJr64 <98753119+LarryJr64@users.noreply.github.com>
2023-02-16 20:58:28 +0100

    Update sarcasm_streamlit.py

--- sarcasm_streamlit.py
+++ sarcasm_streamlit.py
@@ -5,7 +5,7 @@ import spacy
 import requests
 from streamlit_lottie import st_lottie
 from io import BytesIO
-import en_core_web_sm
+pip3 install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.0/en_core_web_sm-2.2.0.tar.gz
 nlp = spacy.load("en_core_web_sm")
 
 
